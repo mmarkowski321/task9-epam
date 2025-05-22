b92d85dcb79ec286c458a20570d16f5df748ae8f
Miłosz Markowski <markowskimilosz989@gmail.com>
2025-05-23 01:26:12 +0200

    dddd

--- task09/locals.tf
+++ task09/locals.tf
@@ -1,6 +1,3 @@
 locals {
-  prefix    = "cmtr-6bbc5609-mod9"
-  rg_name   = format("%s-rg", local.prefix)
-  vnet_name = format("%s-vnet", local.prefix)
-  location  = "East US"
+  location = "East US"
 }
--- task09/modules/afw/locals.tf
+++ task09/modules/afw/locals.tf
@@ -1,45 +1,5 @@
 locals {
-  prefix = "cmtr-6bbc5609-mod9"
-
-  afw_name     = format("%s-afw", local.prefix)
-  afw_pip_name = format("%s-pip", local.prefix)
-  afw_rt_name  = format("%s-rt", local.prefix)
-
-  app_rule_collection_name = "appRules"
-  net_rule_collection_name = "netRules"
-  nat_rule_collection_name = "natRules"
-  default_route_name       = "egress-via-firewall"
-
-
-  app_rules = [
-    {
-      name             = "AllowMicrosoftUpdates"
-      source_addresses = ["*"]
-      target_fqdns     = ["*.microsoft.com"]
-      port             = 443
-      type             = "Https"
-    }
-  ]
-
-  net_rules = [
-    {
-      name                  = "AllowDNS"
-      source_addresses      = ["*"]
-      destination_addresses = ["*"]
-      destination_ports     = ["53"]
-      protocols             = ["UDP"]
-    }
-  ]
-
-  nat_rules = [
-    {
-      name                  = "nginx"
-      source_addresses      = ["*"]
-      destination_ports     = ["80"]
-      translated_port       = 80
-      translated_address    = var.aks_private_ip
-      destination_addresses = [azurerm_public_ip.afw_pip.ip_address]
-      protocols             = ["TCP"]
-    }
-  ]
+  afw_name     = "cmtr-6bbc5609-mod9-afw"
+  afw_pip_name = "cmtr-6bbc5609-mod9-pip"
+  afw_rt_name  = "cmtr-6bbc5609-mod9-rt"
 }
