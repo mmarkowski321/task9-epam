0cb1ec5662f3bc800704c4b9cd4b9fef5d8c07d8
Miłosz Markowski <markowskimilosz989@gmail.com>
2025-05-23 00:58:44 +0200

    task93

--- task09/modules/afw/locals.tf
+++ task09/modules/afw/locals.tf
@@ -38,7 +38,7 @@ locals {
       destination_ports     = ["80"]
       translated_port       = 80
       translated_address    = var.aks_private_ip
-      destination_addresses = var.aks_loadbalancer_ip
+      destination_addresses = [var.aks_loadbalancer_ip]
       protocols             = ["TCP"]
     }
   ]
