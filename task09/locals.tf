4fd61102761d67003832926d6ac3d4c3093564f3
Miłosz Markowski <markowskimilosz989@gmail.com>
2025-05-23 02:23:02 +0200

    task9

--- task09/locals.tf
+++ task09/locals.tf
@@ -1,3 +1,6 @@
 locals {
-  location = "East US"
+  prefix    = "cmtr-6bbc5609-mod9"
+  rg_name   = format("%s-rg", local.prefix)
+  vnet_name = format("%s-vnet", local.prefix)
+  location  = "East US"
 }
