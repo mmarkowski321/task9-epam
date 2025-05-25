1108d27b3e5e54c545927d55d3a09deb94183c7b
Miłosz Markowski <markowskimilosz989@gmail.com>
2025-05-25 12:07:16 +0200

    task99999999999999999

--- task09/modules/afw/locals.tf
+++ task09/modules/afw/locals.tf
@@ -27,13 +27,6 @@ locals {
       destination_addresses = ["*"]
       destination_ports     = ["53"]
       protocols             = ["UDP"]
-    },
-    {
-      name                  = "AllowHTTP"
-      source_addresses      = ["*"]
-      destination_addresses = [var.aks_private_ip]
-      destination_ports     = ["80"]
-      protocols             = ["TCP"]
     }
   ]
 
@@ -43,7 +36,7 @@ locals {
       source_addresses   = ["*"]
       destination_ports  = ["80"]
       translated_port    = 80
-      translated_address = var.aks_private_ip
+      translated_address = var.aks_loadbalancer_ip
       protocols          = ["TCP"]
     }
   ]
--- task09/terraform.tfvars
+++ task09/terraform.tfvars
@@ -1,3 +1,3 @@
 aks_subnet_name     = "aks-snet"
-aks_loadbalancer_ip = "134.33.219.42"
+aks_loadbalancer_ip = "51.8.213.25"
 aks_private_ip      = "10.0.0.10"
